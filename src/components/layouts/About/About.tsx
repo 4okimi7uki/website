e2d869443ab008ddcc30dd39b08d2d751bfcfb5f
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-10-26 23:30:12 +0900

    feat: add layout scaffold

---
+++ src/components/layouts/About/About.tsx
@@ -0,0 +1,4 @@
+import styles from "./index.module.scss";
+export function About() {
+    return <div>About あいうえお</div>;
+}
---
+++ src/components/layouts/Top/Top.tsx
@@ -0,0 +1,4 @@
+import styles from "./index.module.scss";
+export function Top() {
+    return <div className={styles.top}>Top</div>;
+}
