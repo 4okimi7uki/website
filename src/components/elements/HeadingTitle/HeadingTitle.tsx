c2752f4f5718a373309ab42dce2f4cf3b0929731
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-10-28 21:55:11 +0900

    feat: add HeadingTitle

--- src/components/elements/HeadingTitle/HeadingTitle.tsx
+++ src/components/elements/HeadingTitle/HeadingTitle.tsx
@@ -1,9 +1,25 @@
+import clsx from "clsx";
 import styles from "./index.module.scss";
 
-type props = {
+type Props = {
     title: string;
-    type: "h1" | "h2" | "h3";
+    type: "h1" | "h2" | "h3" | "h4";
 };
-export function HeadingTitle({ title, type }: props) {
-    return <div className={styles.title}>{title}</div>;
+
+function selectHead({ type, title }: Props) {
+    switch (type) {
+        case "h1":
+            return <h1 className={clsx(styles.headTitle, styles.h1)}>{title}</h1>;
+        case "h2":
+            return <h2 className={clsx(styles.headTitle, styles.h2)}>{title}</h2>;
+        case "h3":
+            return <h3 className={clsx(styles.headTitle, styles.h3)}>{title}</h3>;
+        case "h4":
+            return <h4 className={clsx(styles.headTitle, styles.h4)}>{title}</h4>;
+        default:
+            return <h1 className={clsx(styles.headTitle, styles.h1)}>{title}</h1>;
+    }
+}
+export function HeadingTitle({ title, type }: Props) {
+    return selectHead({ type, title });
 }
