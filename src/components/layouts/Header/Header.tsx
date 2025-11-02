e36cfb97754a37c49a43d4b2d7c4502ff1561030
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-11-02 22:42:12 +0900

    feat: add header navigation contents

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,6 +1,33 @@
-import React from "react";
 import styles from "./index.module.scss";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import MyIcon from "@/assets/svg/girigiri.svg";
 
 export function Header() {
-    return <div className={styles.header}>Header</div>;
+    return (
+        <div className={styles.header}>
+            <div className={styles.container}>
+                <div className={styles.burger}>
+                    <button>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
+                <ul className={styles.navList}>
+                    <li>
+                        <Link href={"https://github.com/4okimi7uki"} target="blank">
+                            <FaGithub className={styles.navIcon} />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={"https://x.com/0000ff_ki"} target="blank">
+                            <FaXTwitter className={styles.navIcon} />
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
