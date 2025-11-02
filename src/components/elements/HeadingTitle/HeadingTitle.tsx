921be46139695c5440b674e70a9f0538ba483132
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-11-02 22:40:31 +0900

    refactor: update HeadingTitle component logic

--- src/components/elements/HeadingTitle/HeadingTitle.tsx
+++ src/components/elements/HeadingTitle/HeadingTitle.tsx
@@ -1,25 +1,14 @@
 import clsx from "clsx";
 import styles from "./index.module.scss";
+import { ElementType } from "react";
 
 type Props = {
     title: string;
-    type: "h1" | "h2" | "h3" | "h4";
+    type?: "h1" | "h2" | "h3" | "h4";
+    align?: "center" | "left" | "right";
 };
 
-function selectHead({ type, title }: Props) {
-    switch (type) {
-        case "h1":
-            return <h1 className={clsx(styles.headTitle, styles.h1)}>{title}</h1>;
-        case "h2":
-            return <h2 className={clsx(styles.headTitle, styles.h2)}>{title}</h2>;
-        case "h3":
-            return <h3 className={clsx(styles.headTitle, styles.h3)}>{title}</h3>;
-        case "h4":
-            return <h4 className={clsx(styles.headTitle, styles.h4)}>{title}</h4>;
-        default:
-            return <h1 className={clsx(styles.headTitle, styles.h1)}>{title}</h1>;
-    }
-}
-export function HeadingTitle({ title, type }: Props) {
-    return selectHead({ type, title });
+export function HeadingTitle({ title, type = "h1", align = "left" }: Props) {
+    const Tag = type as ElementType;
+    return <Tag className={clsx(styles.headTitle, styles[type], align && styles[`align-${align}`])}>{title}</Tag>;
 }
