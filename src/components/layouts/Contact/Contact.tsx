e07e921877af59fb3f5688b6cce4509d318a9a59
4okimi7uki <bv16001@shibaura-it.ac.jp>
2026-01-03 20:01:24 +0900

    wip: add contact section

---
+++ src/components/layouts/Contact/Contact.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+import { Section } from '../Section';
+import { HeadingTitle } from '@/components/elements/HeadingTitle';
+
+export function Contact() {
+  return (
+    <Section>
+      <HeadingTitle title="Contact" align="center" />
+    </Section>
+  );
+}
